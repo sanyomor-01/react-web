d6900d78759504fdb77f9ca5fa5caf87b1631f58
sanyomor-01 <sanyomormichael@gmail.com>
2024-02-20 23:55:50 +0000

    styles applied

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,27 @@ import React from "react";
  
 export default function Navbar(props) {
     return(
-        <nav className={props.darkMode ? "dark": ""}>           
+        <nav 
+        className={props.darkMode ? "dark": ""}
+    >
         <img 
-            className="nav--logo_icon"
+            className="nav-logo"
             src="./assets/react-icon-small.png"
-    />
-            <h3 className="nav-icon-text">ReactFacts</h3>
-
-            <div className="toggler"
-             onClick={props.toggleDarkMode}
+        />
+        <h3 className="nav-logo-text">ReactFacts</h3>
+        
+        <div 
+            className="toggler" 
+        >
+            <p className="toggler-light">Light</p>
+            <div 
+                className="toggler-slider"
+                onClick={props.toggleDarkMode}
             >
-                <p className="toggler-light">Light</p>
-                <div className="toggler-slider">
-                    <div className="toggler-circle"></div>
-                </div>
-                <p className="toggler-dark">Dark</p>
+                <div className="toggler-slider-circle"></div>
             </div>
-        </nav>
+            <p className="toggler-dark">Dark</p>
+        </div>
+    </nav>
     )
 }
